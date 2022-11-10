8bb094f81f5a3369bdbf24c1db08931c70d6394d
Joe P Shoulak <joe@squarecompass.io>
2022-11-10 14:31:53 -0600

    implemented missing code

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,14 +1,14 @@
-import { openDB } from 'idb';
+import { openDB } from "idb";
 
 const initdb = async () =>
-  openDB('jate', 1, {
+  openDB("jate", 1, {
     upgrade(db) {
-      if (db.objectStoreNames.contains('jate')) {
-        console.log('jate database already exists');
+      if (db.objectStoreNames.contains("jate")) {
+        console.log("jate database already exists");
         return;
       }
-      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
-      console.log('jate database created');
+      db.createObjectStore("jate", { keyPath: "id", autoIncrement: true });
+      console.log("jate database created");
     },
   });
 
@@ -17,19 +17,19 @@ const initdb = async () =>
 */
 export const putDb = async (content) => {
   // First, create a variable, and set it to asyncronously await the opening of the database. Replace the items in all caps
-  const DB_VAR = await openDB('DB_NAME', 1);
+  const jateDb = await openDB("jate", 1);
 
   // Now create a variable for the transaction
-  const TX_VAR = jateDb.transaction('DB_NAME', 'readwrite');
+  const tx = jateDb.transaction("jate", "readwrite");
 
   // Now create a variable for the store
-  const STORE_VAR = tx.objectStore('DB_NAME');
+  const store = tx.objectStore("jate");
 
   // Now create a variable named "request" and have it perform the update
-  const VAR_NAME = store.put({ id: 1, value: content });
+  const request = store.put({ id: 1, value: content });
 
   const result = await request;
-  console.log('🚀 - data saved to the database', result.value);
+  console.log("🚀 - data saved to the database", result?.value);
 };
 
 /*
@@ -37,15 +37,20 @@ export const putDb = async (content) => {
 */
 export const getDb = async () => {
   // You can duplicate the same first lines of code from above, except that the transaction will be 'readonly'
-  
-  // LINES 1-3 HERE
 
+  const jateDb = await openDB("jate", 1);
+
+  // Now create a variable for the transaction
+  const tx = jateDb.transaction("jate", "readonly");
+
+  // Now create a variable for the store
+  const store = tx.objectStore("jate");
   // Leave the rest as-is
   const request = store.get(1);
   const result = await request;
   result
-    ? console.log('🚀 - data retrieved from the database', result.value)
-    : console.log('🚀 - data not found in the database');
+    ? console.log("🚀 - data retrieved from the database", result.value)
+    : console.log("🚀 - data not found in the database");
   // Check if a variable is defined and if it is, return it. See MDN Docs on Optional Chaining (https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Optional_chaining)
   return result?.value;
 };
